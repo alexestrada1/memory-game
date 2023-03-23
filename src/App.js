250c77e5ec2504f4a02d319d8aae878d642865af
alexestrada1 <estradaalex46@gmail.com>
2023-03-23 18:04:40 -0500

    added score and high score, also the deck shuffles verytime the page is reloded

--- src/App.js
+++ src/App.js
@@ -1,19 +1,55 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import CardDeck from "./Components/CardDeck/CardDeck";
 import shuffle from "./Components/util/shuffle";
+import Score from "./Components/Score/Score";
 const App = () => {
-  const [cardDeck, setCardDeck] = useState([{id: 1, number: 1}, {id: 2,number: 2}, {id: 3,number: 3}, {id: 4,number: 4}])
+  const [cardDeck, setCardDeck] = useState([
+    { id: 1, number: 1 },
+    { id: 2, number: 2 },
+    { id: 3, number: 3 },
+    { id: 4, number: 4 },
+  ]);
+  const [clickedCards, setClickedCards] = useState([]);
+  const [score, setScore] = useState(0);
+  const [highScore, setHighScore] = useState(0);
+  useEffect(() => {
+    if (score > highScore) {
+      setHighScore(score);
+    }
+  }, [score, highScore]);
 
-  const handleCardClick =() =>{
+  useEffect(() => {
+    const newDeck = shuffle([...cardDeck]);
+    setCardDeck(newDeck);
+  }, []);
+
+
+  const handleScore = (card) => {
+    if (clickedCards.includes(card)) {
+      setScore(0);
+      setClickedCards([]);
+    } else {
+      setScore(score + 1);
+    }
+  };
+  const handleCardClick = (e) => {
+    const cardValue = e.currentTarget.getAttribute("value");
+
+    const newClickedCards = [...clickedCards];
+    newClickedCards.push(cardValue);
+    setClickedCards(newClickedCards);
+    handleScore(cardValue);
+
     const newDeck = [...cardDeck];
     shuffle(newDeck);
     setCardDeck(newDeck);
-  }
+  };
   return (
-    <div className="App"> 
-      <CardDeck cards = {cardDeck} clickHandler={handleCardClick}/>
+    <div className="App">
+      <Score score={score} highScore={highScore} />
+      <CardDeck cards={cardDeck} clickHandler={handleCardClick} />
     </div>
   );
-}
+};
 
 export default App;
--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -2,7 +2,7 @@ import React from "react";
 import style from "./Card.module.css"
 const Card = ({ number, clickHandler}) => {
     return(
-        <div className={style.card} onClick={clickHandler}>
+        <div className={style.card} onClick={clickHandler} value={number}>
             <h2>{number}</h2>
             </div>
     )
